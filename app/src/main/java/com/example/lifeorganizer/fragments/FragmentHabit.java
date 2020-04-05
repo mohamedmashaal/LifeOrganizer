fc90be13e2c20e747b9aef9c406de72cb03508c7
MuhammadSharaf <m.sharf47@gmail.com>
2020-04-05 16:14:12 +0200

    Add Habits list

--- app/src/main/java/com/example/lifeorganizer/fragments/FragmentHabit.java
+++ app/src/main/java/com/example/lifeorganizer/fragments/FragmentHabit.java
@@ -5,19 +5,31 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.example.lifeorganizer.Adapters.HabitListAdapter;
+import com.example.lifeorganizer.Backend.AfterGetHabits;
+import com.example.lifeorganizer.Backend.HabitManager;
+import com.example.lifeorganizer.Data.Habit;
 import com.example.lifeorganizer.R;
 import com.example.lifeorganizer.dialogs.AddHabitDialog;
-import com.example.lifeorganizer.dialogs.AddTaskDialog;
 import com.example.lifeorganizer.dialogs.IAddHabitDialog;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class FragmentHabit extends Fragment {
+
+    private ArrayList<Habit> habitList;
+    private RecyclerView mRecyclerView;
+    private HabitListAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private HabitManager habitManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +37,27 @@ public class FragmentHabit extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_habit, container, false);
 
+        habitManager = HabitManager.getInstance(getContext());
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.habits_list_recycler);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
 
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+
+        habitManager.getHabits(new AfterGetHabits() {
+            @Override
+            public void afterGetHabits(List<Habit> habits) {
+                habitList = new ArrayList<>(habits);
+            }
+        });
+
+        mAdapter = new HabitListAdapter(habitList);
+        mRecyclerView.setAdapter(mAdapter);
 
         // Inflate the layout for this fragment
         return rootView;
@@ -33,8 +65,6 @@ public class FragmentHabit extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
-        final TextView textView = (TextView) view.findViewById(R.id.testDialog);
 
         ((FloatingActionButton) view.findViewById(R.id.fb_add_habit)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +74,6 @@ public class FragmentHabit extends Fragment {
 
                     @Override
                     public void onPositiveClicked(String title, String description, String daysMask, int hrsPerWeek, Date startDate) {
-                        textView.setText(daysMask);
                         //TODO call add nwe task here
                     }
                 });
